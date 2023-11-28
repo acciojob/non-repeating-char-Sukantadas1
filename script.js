435d7f4037f650150fdc29e64e2b85ca241c796c
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-28 08:41:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ // Create a map to store the frequency of each character in the string
+  const charFrequency = new Map();
+
+  // Iterate through the string and update the character frequency map
+  for (const char of str) {
+    charFrequency.set(char, (charFrequency.get(char) || 0) + 1);
+  }
+
+  // Find the first non-repeated character in the string
+  for (const char of str) {
+    if (charFrequency.get(char) === 1) {
+      return char;
+    }
+  }
+
+  // Return null if no non-repeated character is found
+  return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
